90c67f1a3c7d8511e58502f410c112d3b5f2faff
Omkar <omkarpawar7000@gmail.com>
2024-08-08 11:12:49 +0530

    updated next config for clerk image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 const { withSentryConfig } = require("@sentry/nextjs");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'img.clerk.com'
+    }]
+  }
+};
 
 module.exports = withSentryConfig(
   nextConfig,
